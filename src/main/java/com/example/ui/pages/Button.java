97a595d0997cee3aa76aa797de18e9992df71b6e
Ivan Khalopik <ikhalopik@gmail.com>
2012-07-01 22:49:46 +0300

    button example added

---
+++ src/main/java/com/example/ui/pages/Button.java
@@ -0,0 +1,14 @@
+package com.example.ui.pages;
+
+import com.example.ui.base.BasePage;
+
+/**
+ * @author Ivan Khalopik
+ * @since 1.0
+ */
+public class Button extends BasePage {
+
+    void onAction() {
+        //do something
+    }
+}
